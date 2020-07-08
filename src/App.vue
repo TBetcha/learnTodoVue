d83058c7aeb1d74fee44516b0db2707e82e39807
Tbetcha <troy.boettger@gmail.com>
2020-07-08 12:39:03 -0400

    add todo method

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,90 @@
+<!-- @format -->
+
 <template>
-  <div id="app">
-    <Header />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
-  </div>
+	<div id="app">
+		<Header />
+		<AddTodo v-on:add-todo="addTodo" />
+		<Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+	</div>
 </template>
 
 <script>
 import Header from './components/layout/Header'
-import Todos from './components/Todos';
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 export default {
-  name: 'App',
-  components:{
-    Header,
-    Todos
-  },
-  data(){
-    return{
-      todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: true
-        },
+	name: 'App',
+	components: {
+		Header,
+		Todos,
+		AddTodo,
+	},
+	data() {
+		return {
+			todos: [
+				{
+					id: 1,
+					title: 'Todo One',
+					completed: true,
+				},
 
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: true
-        },
-        {
-          id: 3,
-          title: 'Todo Two',
-          completed: false
-        }
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    }
-  }
+				{
+					id: 2,
+					title: 'Todo Two',
+					completed: true,
+				},
+				{
+					id: 3,
+					title: 'Todo Two',
+					completed: false,
+				},
+			],
+		}
+	},
+	methods: {
+		deleteTodo(id) {
+			this.todos = this.todos.filter((todo) => todo.id !== id)
+		},
+		addTodo(newTodo) {
+			this.todos = [...this.todos, newTodo]
+		},
+	},
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+.btn {
+	display: inline-block;
+	border: none;
+	background: #555;
+	color: #fff;
+	padding: 7px 20px;
+	cursor: pointer;
+}
+
+.btn:hover {
+	background: #666;
+}
+
+form {
+	display: flex;
+}
+
+input[type='text'] {
+	flex: 10;
+	padding: 5px;
+}
+
+input[type='submit'] {
+	flex: 2;
 }
 </style>
